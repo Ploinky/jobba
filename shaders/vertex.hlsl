6547995cd78ab0b60370ba20350fd0dfff895ae0
Ploinky <mail@johannes-litger.de>
2022-03-25 21:51:22 +0100

    Added vertex shader

---
+++ shaders/vertex.hlsl
@@ -0,0 +1,8 @@
+cbuffer perObjectBuffer {
+    float4x4 worldViewMatrix;
+}
+
+void main(float3 position : POSITION, float4 color : COLOR, out float4 positionOut : SV_POSITION, out float4 colorOut : COLOR) {
+    positionOut = mul(float4(position, 1.0f), worldViewMatrix);
+    colorOut = color;
+}
