115a86feb4b027595372722ec84449e219104acc
Ploinky <mail@johannes-litger.de>
2022-05-03 23:22:18 +0200

    Set all camera variables in constructor

--- src/camera.cpp
+++ src/camera.cpp
@@ -5,5 +5,7 @@ namespace P3D {
         this->fov = 55;
         this->nearClip = 0.001f;
         this->farClip = 1000.0f;
+        this->position = DirectX::XMFLOAT3();
+        this->rotation = DirectX::XMFLOAT3();
     }
 }
